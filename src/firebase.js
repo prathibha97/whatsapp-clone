d66ebd78ee68f7a4bd1f128e6c1a25bad923a22e
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-06 17:53:28 +0530

    firebase config added

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZCIfNryMygUllyTeDt-H4Pxw_lrgmxaA",
+    authDomain: "whatsapp-clone-35493.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-35493.firebaseio.com",
+    projectId: "whatsapp-clone-35493",
+    storageBucket: "whatsapp-clone-35493.appspot.com",
+    messagingSenderId: "817384229971",
+    appId: "1:817384229971:web:99d2e04f4cbaccf2b74aa6"
+  };
