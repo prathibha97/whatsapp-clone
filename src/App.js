b31d82d04596282868aa209bad2a5f812cb4b8c6
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-07 21:54:28 +0530

    pusher added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
 
 function App() {
+  useEffect(()=>{
+    const pusher = new Pusher('70e47dcea78ebf55fb79', {
+      cluster: 'mt1'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted',(data)=> {
+      alert(JSON.stringify(data));
+    });
+  },[])
   return (
     <div className='app'>
       <div className='app_body'>
