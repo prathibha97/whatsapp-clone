7d0e8eff1b855068938b23986f5ab67fd40da99e
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-07 22:41:40 +0530

    axios & hooks added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
-import Pusher from 'pusher-js'
+import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
-  useEffect(()=>{
-    
-  },[])
-  useEffect(()=>{
-    const pusher = new Pusher('70e47dcea78ebf55fb79', {
-      cluster: 'mt1'
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+  console.log(messages);
+
+  useEffect(() => {
+    const pusher = new Pusher("70e47dcea78ebf55fb79", {
+      cluster: "mt1",
     });
 
-    const channel = pusher.subscribe('messages');
-    channel.bind('inserted',(data)=> {
-      alert(JSON.stringify(data));
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (newMessage) => {
+      setMessages([...messages, newMessage])
     });
-  },[])
+    return ()=>{
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+  }, [messages]);
   return (
     <div className='app'>
       <div className='app_body'>
