72d5886a83bba2e060fe45e34688181861ad4b43
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-07 11:50:53 +0530

    chat styled

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./Chat.css";
-import { Avatar , IconButton} from "@material-ui/core";
-import {AttachFile, MoreVert, SearchOutlined} from '@material-ui/icons'
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+  AttachFile,
+  MoreVert,
+  SearchOutlined,
+  InsertEmoticon,
+  Mic,
+} from "@material-ui/icons";
 
 function Chat() {
   return (
@@ -23,6 +29,31 @@ function Chat() {
             <MoreVert />
           </IconButton>
         </div>
+      </div>
+      <div className='chat_body'>
+        <p className='chat_message'>
+          <span className='chat_name'>Prathibha</span>
+          This is a message
+          <span className='chat_timestamp'>{new Date().toUTCString()}</span>
+        </p>
+        <p className='chat_receiver chat_message'>
+          <span className='chat_name'>Prathibha</span>
+          This is a message
+          <span className='chat_timestamp'>{new Date().toUTCString()}</span>
+        </p>
+        <p className='chat_message'>
+          <span className='chat_name'>Prathibha</span>
+          This is a message
+          <span className='chat_timestamp'>{new Date().toUTCString()}</span>
+        </p>
+      </div>
+      <div className='chat_footer'>
+        <InsertEmoticon />
+        <form>
+          <input type='text' placeholder='Type a message' />
+          <button type='submit'>Send message</button>
+        </form>
+        <Mic />
       </div>
     </div>
   );
