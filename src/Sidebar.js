a5895bd3d0213455145405e8284a4e2c2f95858d
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-07 09:59:54 +0530

    sidebar added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import './Sidebar.css'
+import React from "react";
+import "./Sidebar.css";
+import ChatIcon from "@material-ui/icons/Chat";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import { IconButton, Avatar } from "@material-ui/core";
 
 function Sidebar() {
-    return (
-        <div className='sidebar'>
-            <h1>Sidebar</h1>
-            <div className="sidebar_header">
-                <div className="sidebar_headerRight">
-                    
-                </div>
-            </div>
+  return (
+    <div className='sidebar'>
+      <div className='sidebar_header'>
+        <Avatar src='https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png' />
+        <div className='sidebar_headerRight'>
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
-    )
+      </div>
+      <div className="sidebar_search">
+          <div className="sidebar_searchContainer">
+              <SearchOutlinedIcon />
+              <input type="text" placeholder='Search or start new chat'/>
+          </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
